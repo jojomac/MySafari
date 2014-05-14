3da428010d5899d6e0294e39fdcf3a03ce198ecc
jojomac <joanne.mcnamee@d214.org>
2014-05-14 16:32:53 -0500

    Our final

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,22 +8,103 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate, UIAlertViewDelegate, UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIButton *forwardButton;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+
+@property (weak, nonatomic) IBOutlet UITextField *myURLTextField;
+@property (weak, nonatomic) IBOutlet UIWebView *myWebView;
+
+
 
 @end
 
 @implementation ViewController
+- (IBAction)onForwardButtonPressed:(id)sender {
+    [self.myWebView goForward];
+    }
+
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.backButton setEnabled:NO];
+    [self.forwardButton setEnabled:NO];
+    self.myWebView.scrollView.delegate=self;
+    
+	}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+
+    [self.backButton setEnabled:([webView canGoBack])];
+    [self.forwardButton setEnabled:([webView canGoForward])];
+
+}
+- (IBAction)onBackButtonPressed:(id)sender
+{
+
+
+    [self.myWebView goBack];
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)onStopLoadingButtonPressed:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.myWebView stopLoading];
+
+
 }
+
+- (IBAction)onReloadButtonPressed:(id)sender
+{
+    [self.myWebView reload];
+}
+- (IBAction)onTeaserButtonPressed:(id)sender
+{
+    UIAlertView *alert = [[UIAlertView alloc] init];
+    alert.title = @"Hello to Ryan";
+    alert.message = @"Coming Soon!";
+    [alert addButtonWithTitle:@"OK"];
+    alert.delegate = self;
+    [alert show];
+
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+if (self.myWebView.scrollView.contentOffset.y==0)
+{
+    [self.myURLTextField setHidden:YES];
+}
+else{
+    [self.myURLTextField setHidden:NO];
+}
+
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSString *urlString = self.myURLTextField.text;
+   if (![urlString hasPrefix:@"http://"])
+   {
+        urlString = [@"http://" stringByAppendingString:urlString];
+   }
+
+    NSURL *url =[NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.myWebView loadRequest:request];
+    [textField resignFirstResponder];
+    return YES;
+
+}
+
+
+
+
+
+
+
 
 @end
